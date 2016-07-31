a95eead0bc2352fd09e130f20d446a88067ab556
samaparo <mirthquakes@gmail.com>
2016-07-31 12:55:37 -0400

    serve all static files out of build directory

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('hello there');
+app.get(/^(.+)$/, function(req, res){
+  res.sendfile( __dirname + '/build/' + req.params[0]);
 });
 
 app.listen(process.env.OPENSHIFT_NODEJS_PORT || 80, process.env.OPENSHIFT_NODEJS_IP || '10.0.0.11', function() {
-  console.log("Go ahead, Atlanta. I'm listening...");
+  console.log("Go ahead, internet. I'm listening...");
 });
